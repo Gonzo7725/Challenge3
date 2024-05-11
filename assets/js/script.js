fe879d33816a33b1359892e9658852bfcbb0e522
Matthew Gonzalez <matthewgonzalez7725@gmail.com>
2024-05-11 13:12:03 -0400

    added new folder and files and starter code

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  var passwordSize=prompt("Enter password length between 8 and 128")
+  console.log(passwordSize)
+
+  if(passwordSize >= 8 && passwordSize <= 128){
+    var confirmLowerCase=confirm("Do you want lower case included in your password?");
+    console.log(confirmLowerCase);
+
+  }
+  else{
+    alert("Invalid Entry, Password length must be between 8 and 128");
+  }
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
